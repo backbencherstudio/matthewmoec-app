4f97191737508f895116768d3dd204b61e0a787f
Nur <alfuad.swe@gmail.com>
2026-04-06 09:41:18 +0600

    feat: implement go_router navigation structure and bottom navigation feature scaffolding

---
+++ lib/core/routes/app_route_paths.dart
@@ -0,0 +1,10 @@
+class AppRoutePaths {
+  static const String splash = '/splash';
+  static const String bottomNav = '/';
+  static const String home = '/home';
+  static const String howItWorks = '/howItWorks';
+  static const String storeDetails = '/storeDetails';
+  static const String externalBrowser = '/externalBrowser';
+  static const String charity = '/charity';
+  static const String share = '/share';
+}
